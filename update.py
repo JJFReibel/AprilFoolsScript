3e0e9f768e25beb97634f80783f8c97b9338093e
Jean-Jacques François Reibel <9219877+JJFReibel@users.noreply.github.com>
2021-09-03 14:47:01 -0700

    Updated to increase the mockery
    ¯\_(ツ)_/¯

--- update.py
+++ update.py
@@ -4,9 +4,8 @@ import os
 # I will not be held responsible for:
 # any shenanigans
 
-os.system("printf '\e[1;31;5;5m WARNING!'")
-
-os.system("printf '\e[1;31;5;5m YOUR SYSTEM IS INFECTED\n'")
-
-os.system("printf '\e[1;31;5;5m FULL SYSTEM SHUTDOWN!\n'")
-
+os.system("printf '\e[1;31;5;5m WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n'")
+os.system("printf '\e[0;33;1;1m YOUR SYSTEM IS INFECTED\n'")
+os.system("printf '\e[0;33;1;1m FULL SYSTEM SHUTDOWN!\n'")
+os.system("printf '\e[0;34;1;1m                     ¯\_(ツ)_/¯\n'")
+os.system("printf '\e[1;31;5;5m WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n'")
