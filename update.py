92ab07d2b03f4361a476c5f69397d83eba5ea9f2
Jean-Jacques François Reibel <9219877+JJFReibel@users.noreply.github.com>
2021-09-03 15:06:19 -0700

    Way more fun

--- update.py
+++ update.py
@@ -4,8 +4,14 @@ import os
 # I will not be held responsible for:
 # any shenanigans
 
-os.system("printf '\e[1;31;5;5m WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n'")
-os.system("printf '\e[0;33;1;1m YOUR SYSTEM IS INFECTED\n'")
-os.system("printf '\e[0;33;1;1m FULL SYSTEM SHUTDOWN!\n'")
+os.system("printf '\e[1;31;5;5m WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n\n'")
+os.system("printf '\e[0;33;1;1m YOUR SYSTEM IS INFECTED.\n'")
+os.system("printf '\e[0;36;1;1m PYTHON WORM IS DOWNLOADING AND CORRUPTING ALL DATA \n'")
+os.system("printf '\e[0;35;1;1m FOLLOWED BY FULL SYSTEM SHUTDOWN.\n\n'")
+os.system("printf '\e[0;31;1;1m ~>'")
+os.system("printf '\e[0;33;1;1m°'")
+os.system("printf '\e[1;32;1;1m)～～～～～～～～～～～～～～～～～～～～～～～～～～～～～\n\n'")
+
 os.system("printf '\e[0;34;1;1m                     ¯\_(ツ)_/¯\n'")
+os.system("printf '\e[0;34;1;1m                     WE OWN YOU. \n\n'")
 os.system("printf '\e[1;31;5;5m WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n'")
