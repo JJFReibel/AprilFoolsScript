60764b7aefd3004e375c8251b644365b3113c5f9
Jean-Jacques François Reibel <9219877+JJFReibel@users.noreply.github.com>
2021-09-03 13:53:52 -0700

    Uploaded update.py
    If someone knows how to use shell and call Python code, but does not check scripts, this should wake them up.

---
+++ update.py
@@ -0,0 +1,12 @@
+import os
+# April's Fools Script
+# By Jean-Jacques F. Reibel
+# I will not be held responsible for:
+# any shenanigans
+
+os.system("printf '\e[1;31;5;5m WARNING!'")
+
+os.system("printf '\e[1;31;5;5m YOUR SYSTEM IS INFECTED\n'")
+
+os.system("printf '\e[1;31;5;5m FULL SYSTEM SHUTDOWN!\n'")
+
