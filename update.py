0c2bbdaeb4d59db625125655bce3a4a49edd694e
Jean-Jacques François Reibel <9219877+JJFReibel@users.noreply.github.com>
2021-09-03 16:36:10 -0700

    Update update.py

--- update.py
+++ update.py
@@ -4,11 +4,21 @@ import os
 # I will not be held responsible for:
 # any shenanigans
 
-os.system("printf '\n\e[1;31;5;5m WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n\n'")
+import os
+import platform
+import getpass
+
+os.system("printf '\n\e[1;31;5;5m WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n'")
 os.system("printf '\e[0;33;1;1m YOUR SYSTEM IS INFECTED.\n'")
 os.system("printf '\e[0;36;1;1m PYTHON WORM IS DOWNLOADING AND CORRUPTING ALL DATA \n'")
-os.system("printf '\e[0;35;1;1m FOLLOWED BY FULL SYSTEM SHUTDOWN.\n'")
-os.system("printf '\e[1;32;1;1m\n\n～～～～～～～～～～～～～～～～～～～～～～～～～～～～～('")
+os.system("printf '\e[0;35;1;1m FOLLOWED BY FULL SYSTEM SHUTDOWN.\n\n'")
+os.system("printf '\e[0;31;1;1m'")
+print("Computer Name:"+ " "*19 + str(platform.node()))
+print(str("Operating System:" + " "*16 + platform.system()))
+print(str("Operating System Release:" + " "*8 + platform.release()))
+print(str("Operating System Version:" + " "*8 + platform.platform()))
+print(str("User Name:" + " "*23) + getpass.getuser())
+os.system("printf '\e[1;32;1;1m\n～～～～～～～～～～～～～～～～～～～～～～～～～～～～～('")
 os.system("printf '\e[0;33;1;1m°'")
 os.system("printf '\e[0;31;1;1m<'")
 os.system("printf '\e[0;31;1;5m~\n'")
@@ -24,5 +34,5 @@ os.system("printf '\e[0;34;1;1m                     WE OWN YOU. \n\n'")
 os.system("printf '\e[0;31;1;5m ~'")
 os.system("printf '\e[0;31;1;1m>'")
 os.system("printf '\e[0;33;1;1m°'")
-os.system("printf '\e[1;32;1;1m)～～～～～～～～～～～～～～～～～～～～～～～～～～～～～\n\n\n'")
+os.system("printf '\e[1;32;1;1m)～～～～～～～～～～～～～～～～～～～～～～～～～～～～～\n\n'")
 os.system("printf '\e[1;31;5;5m WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n\n'")
